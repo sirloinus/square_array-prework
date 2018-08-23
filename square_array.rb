2c5a20318a234846af924d7028ba281f4497c1e9
Amalie S <amaliesmidth@gmail.com>
2018-08-23 13:57:35 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,17 @@
+# def square_array(numbers)
+#   squared_array = []
+#   numbers.each do |square_number|
+#     square_number = square_number**2
+#     squared_array.push(square_number)
+#     squared_array
+#   end
+# end
+
+
 def square_array(array)
-  # your code here
+  sqarray = []
+  array.each do |number|
+    sqarray << number**2
+    sqarray
+  end
 end
